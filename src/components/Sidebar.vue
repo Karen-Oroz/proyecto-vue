4c0b86b6cc1a5cfa8c6681c9e1e70bc76c1539a9
karenoroz <a23311032@uthermosillo.edu.mx>
2025-02-04 23:14:37 -0700

    crea componentes lista de tareas. crea rutas. etc

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="sidebar">
+    <h2>Barra lateral</h2>
+    <ul>
+    <li><Router-link to="/contador">Contador</Router-link></li>
+    <li><Router-link to="/lita-de-tareas">Lista De Tareas</Router-link></li>
+    </ul>
+
+
+
+</div>
+
+
+
+</template>
+
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar {
+    width: 200px;
+    background-color: #f0f0f0;
+    padding: 20px;
+}
+
+</style>
