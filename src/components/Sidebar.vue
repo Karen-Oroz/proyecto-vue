a9c74a64e5c57084a496f0ba9944fdad1d865150
karenoroz <a23311032@uthermosillo.edu.mx>
2025-02-05 21:14:55 -0700

    crea store de pinia para almacenar datos

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
 <div class="sidebar">
-    <h2>Barra lateral</h2>
+    <h2>bienvenido</h2>
+    <h3>Nombre: {{ nombre }}</h3>
+    <h3>Email: {{ email }}</h3>
     <ul>
     <li><Router-link to="/contador">Contador</Router-link></li>
     <li><Router-link to="/lita-de-tareas">Lista De Tareas</Router-link></li>
@@ -18,6 +20,17 @@
 
 
 <script setup>
+import { useRegistrarStore } from '@/modules/registro/stores/registrarStore';
+import {ref, watch} from 'vue';
+const registrarStore = useRegistrarStore();
+const nombre = ref(registrarStore.nombre.value);
+const email = ref(registrarStore.email.value);
+watch(() => registrarStore.nombre, (newValue) => {
+    nombre.value = newValue;
+});
+watch(() => registrarStore.email, (newValue) => {
+    email.value = newValue;
+})
 
 </script>
 
